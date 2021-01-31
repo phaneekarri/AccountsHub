db9aa32105ecb086ff5a0f7c5e04a105b651717f
phaneekarri <phaneekarri@gmail.com>
2021-01-31 03:43:39 -0500

    Create Solution

---
+++ Entities/Entities/LoanContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Entities
+{
+    public class LoanContext : DbContext
+    {
+        public LoanContext(DbContextOptions options) : base(options)
+        {
+
+        }
+    }
+}

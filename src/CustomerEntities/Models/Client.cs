9462499a6b101271ac97da8041032783508053ed
phaneekarri <phaneekarri@gmail.com>
2021-02-14 23:25:22 -0500

    Client changes

--- src/CustomerEntities/Models/Client.cs
+++ src/CustomerEntities/Models/Client.cs
@@ -1,4 +1,6 @@
 ﻿using CustomerEntities.Models.Contacts;
+using CustomerEntities.Models.Types;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 
@@ -9,37 +11,39 @@ namespace CustomerEntities.Models
         public int Id { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
+        public DateTime DOB { get; set; }
 
+        public int Age { get 
+            {
+                var age = DateTime.Today.Year - DOB.Year;
+                if (DateTime.Today.Day > DOB.Day) age++;
+                return age;
+            }
+        }
         public ClientEmailContact PrimaryEmail
         {
             get => EmailContacts.SingleOrDefault(x => x.ContactType.Description == "Primary");
-            private set { }
         }
         public ClientAddressContact PrimaryAddress
         {
             get => AddressContacts.SingleOrDefault(x => x.ContactType.Description == "Primary");
-            private set { }
         }
         public ClientPhoneContact PrimaryPhone
         {
             get => PhoneContacts.SingleOrDefault(x => x.ContactType.Description == "Primary");
-            private set { }
         }
 
         public ICollection<ClientEmailContact> SecondaryEmail
         {
             get => EmailContacts.Where(x => x.ContactType.Description == "Secondary").ToHashSet();
-            private set { }
         }
         public ICollection<ClientAddressContact> SecondaryAddresses
         {
             get => AddressContacts.Where(x => x.ContactType.Description == "Secondary").ToHashSet();
-            private set { }
         }
         public ICollection<ClientPhoneContact> SecondaryPhone
         {
             get => PhoneContacts.Where(x => x.ContactType.Description == "Secondary").ToHashSet();
-            private set { }
         }
 
         public ICollection<AccountOwner> Accounts { get; set; }= new HashSet<AccountOwner>();
@@ -47,10 +51,36 @@ namespace CustomerEntities.Models
         public ICollection<ClientAddressContact> AddressContacts { get; set; } = new HashSet<ClientAddressContact>();
         public ICollection<ClientPhoneContact> PhoneContacts { get; set; } = new HashSet<ClientPhoneContact>();
 
-        private void AddPrimaryContact<T>(ICollection<T> contacts, T primaryContact ) 
-            //where T : Contact
+        public void AddPrimaryEmail(string Email ) 
         {
-          // contacts.Where(x => x.)Contains() 
+            var existingPrimary = EmailContacts.Single(c => c.ContactType.Description == "Primary");
+            existingPrimary.ContactType = new SecondaryContact();
+            EmailContacts.Add(new ClientEmail<PrimaryContact>() { Client = this , Value = Email});
+        }
+        public void AddPrimaryPhone(string Phone)
+        {
+            var existingPrimary = PhoneContacts.Single(c => c.ContactType.Description == "Primary");
+            existingPrimary.ContactType = new SecondaryContact();
+            PhoneContacts.Add(new ClientPhone<PrimaryContact>() { Client = this, Value = Phone });
+        }
+        public void AddPrimaryAddress(Address Address)
+        {
+            var existingPrimary = AddressContacts.Single(c => c.ContactType.Description == "Primary");
+            existingPrimary.ContactType = new SecondaryContact();
+            AddressContacts.Add(new ClientAddress<PrimaryContact>() { Client = this, Value = Address });
+        }
+
+        public void AddSecondaryEmail(string Email)
+        {
+            EmailContacts.Add(new ClientEmail<SecondaryContact>() { Client = this, Value = Email });
+        }
+        public void AddSecondaryPhone(string Phone)
+        {
+            PhoneContacts.Add(new ClientPhone<SecondaryContact>() { Client = this, Value = Phone });
+        }
+        public void AddSecondaryAddress(Address Address)
+        {
+            AddressContacts.Add(new ClientAddress<SecondaryContact>() { Client = this, Value = Address });
         }
 
     }

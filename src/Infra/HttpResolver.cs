e06a1556b2a2898e90eb21041781ba6cdb81898e
phaneekarri <phaneekarri@gmail.com>
2024-07-14 18:18:54 -0400

    Add Infra project

---
+++ src/Infra/HttpResolver.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Http;
+
+namespace Infra;
+
+public interface IHttpResolver<T>{
+    T Get ();
+}
+public abstract class HttpResolver<T>
+{
+   protected readonly IHttpContextAccessor Context;
+
+   public HttpResolver(IHttpContextAccessor context)
+   {
+     Context = context;
+   }
+   public abstract T Get();
+}
+
+public interface IUserResolver : IHttpResolver<string?>{}
+
+public class UserResolver : HttpResolver<string?> ,IUserResolver
+{
+    public UserResolver(IHttpContextAccessor context) : base(context){}
+    public override string? Get() => Context?.HttpContext?.User?.Identity?.Name;
+}
